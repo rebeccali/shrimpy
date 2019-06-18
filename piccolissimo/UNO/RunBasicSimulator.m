2541ab5b92f52f39e64ea908047e1d5361fb5405
Rebecca Li <rmli@protonmail.ch>
2019-06-18 13:48:45 -0400

    Added Run functions for the simulator and beta D

---
+++ piccolissimo/UNO/RunBasicSimulator.m
@@ -0,0 +1,14 @@
+%% Run a simple simulation for Piccolissimo that works.
+%  Copyright Modlab 2019
+%  Author: Rebecca Li
+
+close all;
+clear all;
+% Add all folders
+addpath(genpath('./'));
+
+% Initialize global parameters Piccolissimo
+Piccolissimo_V11
+
+% Run many simulations 
+OptimizeBetaD
