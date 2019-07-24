84e124382b15931151e2337a2df276de736e718f
Rebecca Li <rmli@protonmail.ch>
2019-07-24 10:46:21 -0400

    Saving before reversing all b2w to w2b

--- piccolissimo/UNO/Simulator/Functions/FlyerODE.m
+++ piccolissimo/UNO/Simulator/Functions/FlyerODE.m
@@ -1,7 +1,24 @@
 function dX = FlyerODE(t,X)
 
 global v_clamp r_b nu_style nu Vcg omg omg_r omg_b angles angle_r angle_s pitch1_d F_p M_p F_d M_d i_m v pwm K_t_m r_m Rb_f m g Is_s Ir_r Rr_f I_tot motor_offset
-
+   % (1): nu is the inflow velocity assumed always perpendicular to blade
+    % (2): u is the x velocity in the flyer frame
+    % (3): v is the y velocity in the flyer frame
+    % (4): w is the z velocity in the flyer frame
+    % (5): p is the x roll rate in the flyer frame
+    % (6): q is the y pitch rate in the flyer frame
+    % (7): r is the z yaw rate in the flyer frame
+    % (8): omg_r is the rotor yaw rate wrt the stator frame
+    % (9): omg_b is the body yaw rate wrt the flyer frame
+    % (10): phi is the angle about x from world frame to the flyer frame
+    % (11): theta is the angle about y from world frame to the flyer frame
+    % (12): psi is the angle about z from world frame to the flyer frame
+    % (13): psi_r is the angle about stator frame z to rotor frame
+    % (14): psi_s is the angle about flyer frame z to stator frame
+    % (15): xe is the world x position
+    % (16): ye is the world y position
+    % (17): ze is the world z position (+ z is "down")
+    % (18): i is the motor current
 %         X
         %% State variables
 %         nu = X(1);
@@ -80,6 +97,25 @@ global v_clamp r_b nu_style nu Vcg omg omg_r omg_b angles angle_r angle_s pitch1
         XeDot = Rw_f*Vcg;
 
         wind_noise_acc = [0 0 0];
+
+   % (1): nu is the inflow velocity assumed always perpendicular to blade % nuDot;
+    % (2): u is the x velocity in the flyer frame     % VcgDot(1);
+    % (3): v is the y velocity in the flyer frame     % VcgDot(2);
+    % (4): w is the z velocity in the flyer frame     % VcgDot(3);
+    % (5): p is the x roll rate in the flyer frame     % omgDot(1);
+    % (6): q is the y pitch rate in the flyer frame     % omgDot(2);
+    % (7): r is the z yaw rate in the flyer frame     % 0;
+    % (8): omg_r is the rotor yaw rate wrt the stator frame     % omg_rDot(3);
+    % (9): omg_b is the body yaw rate wrt the flyer frame     % omg_bDot(3) + omgDot(3);
+    % (10): phi is the angle about x from world frame to the flyer frame     % anglesDot(1);
+    % (11): theta is the angle about y from world frame to the flyer frame     % anglesDot(2);
+    % (12): psi is the angle about z from world frame to the flyer frame     % anglesDot(3);
+    % (13): psi_r is the angle about stator frame z to rotor frame     % omg_r;
+    % (14): psi_s is the angle about flyer frame z to stator frame     % omg_b;
+    % (15): xe is the world x position     % XeDot(1)+wind_noise_acc(1);
+    % (16): ye is the world y position     % XeDot(2)+wind_noise_acc(2);
+    % (17): ze is the world z position (+ z is "down")     % XeDot(3)+wind_noise_acc(3);
+    % (18): i is the motor current     % iDot];
 
         dX = [nuDot; VcgDot(1); VcgDot(2); VcgDot(3); omgDot(1); omgDot(2); 0; omg_rDot(3); omg_bDot(3) + omgDot(3); anglesDot(1); anglesDot(2); anglesDot(3); omg_r; omg_b; XeDot(1)+wind_noise_acc(1); XeDot(2)+wind_noise_acc(2); XeDot(3)+wind_noise_acc(3); iDot];
 
